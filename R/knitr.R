c0402b2336f069a040bdca8f92e6c143c4e1f584
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-11-26 17:16:45 +0000

    Start documenting the user-facing parts

--- R/knitr.R
+++ R/knitr.R
@@ -1,3 +1,36 @@
+##' Prepare for use with knitr by registering the `cppdoc` knitr
+##' engine and optionally setting a default package to locate C++
+##' definitions in.  This operation can be undone with
+##' `cppdoc_unregister`.
+##'
+##' @title Prepare for use with knitr
+##'
+##' @param package Name of the package to use
+##'
+##' @return Nothing, called for its side effects
+##'
+##' @export
+##' @examples
+##' cppdoc::cppdoc_register(NULL)
+##' cppdoc::cppdoc_unregister()
+cppdoc_register <- function(package) {
+  ret <- knitr::knit_engines$set(cppdoc = cppdoc_engine)
+  if (!is.null(package)) {
+    index_load(package)
+    knitr::opts_chunk$set(cppdoc_package = package)
+  }
+  invisible(ret)
+}
+
+
+##' @rdname cppdoc_register
+##' @export
+cppdoc_unregister <- function() {
+  knitr::knit_engines$delete("cppdoc")
+  knitr::opts_chunk$delete("cppdoc_package")
+}
+
+
 cppdoc_engine <- function(options) {
   out <- collector()
   for (expr in parse(text = options$code)) {
@@ -6,11 +39,4 @@ cppdoc_engine <- function(options) {
       TRUE)
   }
   out$get()
-}
-
-
-cppdoc_register <- function(package) {
-  knitr::knit_engines$set(cppdoc = cppdoc_engine)
-  index_load(package)
-  knitr::opts_chunk$set(cppdoc_package = package)
 }
