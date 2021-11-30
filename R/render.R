c98bdef1badc7a98ab7023b0e2d81109f7eb6827
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-11-30 20:16:37 +0000

    Start link remapping

--- R/render.R
+++ R/render.R
@@ -1,9 +1,10 @@
-render_function <- function(x) {
+render_function <- function(x, control) {
+  force(control)
   out <- collector()
 
   ## TODO: we should render a better decl here, by rendering
   ## parameters.
-  decl <- sprintf("%s(%s)", x$name, render_params(x$param))
+  decl <- sprintf("%s(%s)", x$name, render_params(x$param, control))
 
   ## NOTE: this is zero for constructors, typically.
   ##
@@ -13,39 +14,41 @@ render_function <- function(x) {
   ## NOTE: I suspect that `const double&` might come out as a vector
   ## here too?
   if (length(x$value) > 0) {
-    decl <- paste(render_linked_text(x$value), decl)
+    decl <- paste(render_linked_text(x$value, control), decl)
   }
 
-  out$add(html_code_block(c(render_tparam(x$tparam), decl)))
+  out$add(html_code_block(c(render_tparam(x$tparam, control), decl)))
 
-  out$add(render_brief(x$brief), TRUE)
-  out$add(render_detail(x$detail), TRUE)
+  out$add(render_brief(x$brief, control), TRUE)
+  out$add(render_detail(x$detail, control), TRUE)
 
   out$get()
 }
 
 
-render_define <- function(x) {
+render_define <- function(x, control) {
+  force(control)
   message("fix rendering of value in render_define")
   browser()
 
   out <- collector()
   out$add(md_code_block(sprintf("#define %s %s", x$name, x$value)))
-  out$add(render_brief(x$brief), TRUE)
-  out$add(render_detail(x$detail), TRUE)
+  out$add(render_brief(x$brief, control), TRUE)
+  out$add(render_detail(x$detail, control), TRUE)
   out$get()
 }
 
 
 render_enum <- function(x) {
+  force(control)
   ## TODO: I *think* but am not sure that 'enum class' vs 'enum'
   ## changes the "strong" field here.
   type <- if (x$strong == "yes") "enum class" else "enum"
   out <- collector()
   out$add(md_code_block(sprintf("%s %s", type, x$name)))
 
-  out$add(render_brief(x$brief), TRUE)
-  out$add(render_detail(x$detail), TRUE)
+  out$add(render_brief(x$brief, control), TRUE)
+  out$add(render_detail(x$detail, control), TRUE)
 
   if (length(x$enumvalues) > 0) {
     out$add("")
@@ -53,24 +56,25 @@ render_enum <- function(x) {
   }
   for (el in x$enumvalues) {
     out$add("")
-    out$add(render_enumvalue(el))
+    out$add(render_enumvalue(el, control))
   }
 
   out$get()
 }
 
 
-render_typedef <- function(x) {
+render_typedef <- function(x, control) {
+  force(control)
   out <- collector()
 
-  tparam <- render_tparam(x$tparam)
+  tparam <- render_tparam(x$tparam, control)
 
   ## TODO: presence of @typedef in the docstrings might break this logic
   ##
   ## TODO: function pointers are hard here, and will require
   ## additional work to render correctly.
   name <- x$name
-  type <- render_linked_text(x$type)
+  type <- render_linked_text(x$type, control)
   if (grepl("^using", x$definition)) {
     definition <- sprintf("using %s = %s", name, type)
   } else {
@@ -78,8 +82,8 @@ render_typedef <- function(x) {
   }
 
   out$add(html_code_block(c(tparam, definition)))
-  out$add(render_brief(x$brief), TRUE)
-  out$add(render_detail(x$detail), TRUE)
+  out$add(render_brief(x$brief, control), TRUE)
+  out$add(render_detail(x$detail, control), TRUE)
 
   out$get()
 }
@@ -87,16 +91,17 @@ render_typedef <- function(x) {
 
 ## TODO: Looks like we're inconsistent throughout as to if we pass
 ## $value or not, would be good to check that.
-render_para <- function(x) {
+render_para <- function(x, control) {
+  force(control)
   out <- collector()
   for (el in x$value) {
     value <- switch(el$type,
                     "text" = el$value,
-                    "computeroutput" = render_computeroutput(el),
-                    "link" = render_link(el),
-                    "parameters" = render_parameters(el),
-                    "simplesect" = render_simplesect(el),
-                    "itemizedlist" = render_itemizedlist(el),
+                    "computeroutput" = render_computeroutput(el, control),
+                    "link" = render_link(el, control),
+                    "parameters" = render_parameters(el, control),
+                    "simplesect" = render_simplesect(el, control),
+                    "itemizedlist" = render_itemizedlist(el, control),
                     stop(sprintf("Unsupported type '%s'", el$type)))
     out$add(value, TRUE)
   }
@@ -104,7 +109,8 @@ render_para <- function(x) {
 }
 
 
-render_computeroutput <- function(x) {
+render_computeroutput <- function(x, control) {
+  force(control)
   ## We only get here if we have complex markup that we can't preserve
   ## properly.  It would be nice to otherwise try and push a link out
   ## as far as possible so that if we have
@@ -113,62 +119,85 @@ render_computeroutput <- function(x) {
   ## we can lift this out to order as ref/code/bold, but not really
   ## sure how best to do that; would need to be within the parse I
   ## think, on the simple markup tags
-  sprintf("<code>%s</code>", render_para(x))
+  sprintf("<code>%s</code>", render_para(x, control))
 }
 
 
-render_link <- function(x, html = FALSE) {
-  if (html) {
-    sprintf('<a href="%s">%s</a>', x$target, x$value)
+render_link_target <- function(target, control) {
+  if (is.null(control$link)) {
+    return(NULL)
+  }
+  i <- match(target, control$link$from)
+  if (is.na(i)) {
+    return(NULL)
+  }
+  control$link$to[[i]]
+}
+
+
+render_link <- function(x, control, html = FALSE) {
+  force(control)
+  target <- render_link_target(x$target, control)
+  browser()
+  if (is.null(target)) {
+    x$value
+  } else if (html) {
+    sprintf('<a href="%s">%s</a>', target, x$value)
   } else {
-    sprintf("[%s](%s)", x$value, x$target)
+    sprintf("[%s](%s)", x$value, target)
   }
 }
 
 
-render_linked_text <- function(x) {
+render_linked_text <- function(x, control) {
+  force(control)
   if (x$type == "linked_text")  {
-    paste(vcapply(x$value, render_linked_text_element), collapse = "")
+    paste(vcapply(x$value, render_linked_text_element, control),
+          collapse = "")
   } else {
-    render_linked_text_element(x)
+    render_linked_text_element(x, control)
   }
 }
 
 
-render_linked_text_element <- function(x) {
+render_linked_text_element <- function(x, control) {
+  force(control)
   switch(x$type,
          text = x$value,
-         link = render_link(x, html = TRUE),
+         link = render_link(x, control, html = TRUE),
          stop("Unexpected linked_list element"))
 }
 
 
-render_class <- function(x) {
+render_class <- function(x, control) {
+  force(control)
   out <- collector()
-  out$add(md_code_block(c(render_tparam(x$tparam),
+  out$add(md_code_block(c(render_tparam(x$tparam, control),
                           paste("class", x$name))))
 
-  out$add(render_brief(x$brief), TRUE)
-  out$add(render_detail(x$detail), TRUE)
+  out$add(render_brief(x$brief, control), TRUE)
+  out$add(render_detail(x$detail, control), TRUE)
 
   for (s in x$sections) {
-    out$add(render_section(s), TRUE)
+    out$add(render_section(s, control), TRUE)
   }
 
   out$get()
 }
 
 
-render_paras <- function(x) {
+render_paras <- function(x, control) {
+  force(control)
   out <- collector()
   for (i in seq_along(x)) {
-    out$add(render_para(x[[i]]), TRUE)
+    out$add(render_para(x[[i]], control), TRUE)
   }
   out$get()
 }
 
 
-render_tparam <- function(x) {
+render_tparam <- function(x, control) {
+  force(control)
   if (is.null(x)) {
     NULL
   } else {
@@ -181,22 +210,24 @@ render_tparam <- function(x) {
 }
 
 
-render_enumvalue <- function(x) {
+render_enumvalue <- function(x, control) {
+  force(control)
   ## TODO: add a div around this, probably around many other things...
   ## TODO: might not want enumerator here?
   c(md_code_block(sprintf("enumerator %s", x$name)),
-    render_paras(x$brief),
-    render_paras(x$detail))
+    render_paras(x$brief, control),
+    render_paras(x$detail, control))
 }
 
 
-render_detail <- function(x) {
+render_detail <- function(x, control) {
+  force(control)
   if (is.null(x)) {
     return(NULL)
   }
   out <- collector()
   for (el in x) {
-    out$add(render_para(el), TRUE)
+    out$add(render_para(el, control), TRUE)
   }
 
   drop_trailing_whitespace(out$get())
@@ -207,7 +238,8 @@ render_detail <- function(x) {
 render_brief <- render_detail
 
 
-render_section <- function(x) {
+render_section <- function(x, control) {
+  force(control)
   info <- switch(
     x$kind,
     "public-type" = list("Public types", render_typedef),
@@ -221,22 +253,24 @@ render_section <- function(x) {
   out <- collector()
   out$add(md_bold(label))
   for (el in x$value) {
-    out$add(render(el), TRUE)
+    out$add(render(el, control), TRUE)
   }
   out$get()
 }
 
 
-render_params <- function(x) {
+render_params <- function(x, control) {
+  force(control)
   args <- vcapply(x, function(el)
-    sprintf("%s %s", render_linked_text(el$type), el$name))
+    sprintf("%s %s", render_linked_text(el$type, control), el$name))
   paste(args, collapse = ", ")
 }
 
 
 ## This is the parameters in the description, not the declaration
 ## (render_params/render_tparams)
-render_parameters <- function(x) {
+render_parameters <- function(x, control) {
+  force(control)
   out <- collector()
   name <- switch(x$kind,
                  "templateparam" = "Template parameters",
@@ -245,7 +279,7 @@ render_parameters <- function(x) {
   out$add(md_bold(name), TRUE)
   for (p in x$value) {
     p_name <- md_bold(paste(md_code(p$name), collapse = ", "))
-    p_str <- render_paras(p$description)
+    p_str <- render_paras(p$description, control)
     stopifnot(length(p_str) > 0)
     p_str[[1]] <- sprintf("* %s: %s", p_name, p_str[[1]])
     ## TODO: check this renders correctly, probably only do the
@@ -263,22 +297,24 @@ render_parameters <- function(x) {
 }
 
 
-render_simplesect <- function(x) {
+render_simplesect <- function(x, control) {
+  force(control)
   out <- collector()
   title <- switch(x$kind,
                   "return" = "Returns",
                   stop("FIXME"))
   ## TODO: this is same logical level as above
   out$add(md_bold(title), TRUE)
-  out$add(render_paras(x$value), TRUE)
+  out$add(render_paras(x$value, control), TRUE)
   out$get()
 }
 
 
-render_itemizedlist <- function(x) {
+render_itemizedlist <- function(x, control) {
+  force(control)
   out <- collector()
   for (el in x$items) {
-    value <- render_paras(el)
+    value <- render_paras(el, control)
     value[[1]] <- paste0("* ", value[[1]])
     if (length(value) > 1L) {
       ## https://www.markdownguide.org/basic-syntax/#unordered-lists
@@ -290,17 +326,19 @@ render_itemizedlist <- function(x) {
 }
 
 
-render_field <- function(x) {
+render_field <- function(x, control) {
+  force(control)
   message("Fix value in render_field")
   browser()
   out <- collector()
   out$add(md_code_block(sprintf("%s %s%s", x$value, x$name, x$args %||% "")))
-  out$add(render_brief(x$brief), TRUE)
-  out$add(render_detail(x$detail), TRUE)
+  out$add(render_brief(x$brief, control), TRUE)
+  out$add(render_detail(x$detail, control), TRUE)
   out$get()
 }
 
 
+## Small utilities here, probably will move file
 md_code_block <- function(code) {
   c("```c++", code, "```")
 }
--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -8,13 +8,9 @@ test_that("can read simple typedef", {
   expect_equal(x$name, "ex::real_type")
   expect_equal(x$definition_short, "using ex::real_type = double")
   expect_equal(x$args, "")
-  brief <- list(list(
-    type = "para",
-    value = list(list(type = "text", value = "This type is documented. "))))
-  expect_equal(x$brief, brief)
   expect_null(x$detail)
 
-  expect_equal(clean_whitespace(render_typedef(x)), ref)
+  expect_equal(clean_whitespace(render_typedef(x, NULL)), ref)
 })
 
 
@@ -31,7 +27,7 @@ test_that("can read undocumented typedef", {
   expect_null(x$brief)
   expect_null(x$detail)
 
-  expect_equal(clean_whitespace(render_typedef(x)), ref)
+  expect_equal(clean_whitespace(render_typedef(x, NULL)), ref)
 })
 
 
@@ -49,7 +45,7 @@ test_that("can read simple function", {
   expect_equal(x$args, "(double x, double y)")
   expect_null(x$brief)
 
-  expect_equal(clean_whitespace(render_function(x)), ref)
+  expect_equal(clean_whitespace(render_function(x, NULL)), ref)
 })
 
 
@@ -68,7 +64,7 @@ test_that("Can render a templated function", {
   expect_equal(x$args, "(const T &x, U y)")
   expect_null(x$brief)
 
-  expect_equal(clean_whitespace(render_function(x)), ref)
+  expect_equal(clean_whitespace(render_function(x, NULL)), ref)
 })
 
 
@@ -82,7 +78,7 @@ test_that("can read simple define", {
   expect_equal(x$value, "1")
   expect_null(x$detail)
 
-  expect_equal(clean_whitespace(render_define(x)), ref)
+  expect_equal(clean_whitespace(render_define(x, NULL)), ref)
 })
 
 
@@ -101,7 +97,7 @@ test_that("can read simple enum", {
                c("apple", "banana", "cherry"))
   expect_null(x$detail)
 
-  expect_equal(clean_whitespace(render_enum(x)), ref)
+  expect_equal(clean_whitespace(render_enum(x, NULL)), ref)
 })
 
 
@@ -118,7 +114,7 @@ test_that("can read simple class", {
   expect_equal(x$sections[[1]]$kind, "public-func")
   expect_null(x$detail)
 
-  expect_equal(clean_whitespace(render_class(x)), ref)
+  expect_equal(clean_whitespace(render_class(x, NULL)), ref)
 })
 
 
@@ -133,7 +129,7 @@ test_that("Can render class fields", {
   expect_length(x$sections, 1)
   expect_equal(x$sections[[1]]$kind, "public-attrib")
 
-  expect_equal(clean_whitespace(render_class(x)), ref)
+  expect_equal(clean_whitespace(render_class(x, NULL)), ref)
 })
 
 
@@ -150,19 +146,56 @@ test_that("Can render class typedefs", {
   expect_equal(x$sections[[1]]$value[[1]]$name, "real_type")
   expect_equal(x$sections[[1]]$value[[2]]$name, "int_type")
 
-  expect_equal(clean_whitespace(render_class(x)), ref)
+  expect_equal(clean_whitespace(render_class(x, NULL)), ref)
 })
 
 
 test_that("linking", {
   path <- doxygen_run_one("examples_hpp/class-link.hpp")
   contents <- data.frame(kind = "class", name = "ex::has_link")
+
+  ## We need to do something here to remap links.
+  ##
+  ## The core class ends here ends up as
+  ## > 'classex_1_1has__link'
+  ## and the
+  ## real_type > classex_1_1has__link_1a02a32e672d966804d3d3d17c8e6bae64
+  ## f         > classex_1_1has__link_1a0a84fcb43ff68b8343502941d7fcaf89
+  ## g         > classex_1_1has__link_1a7e95c4adf201b650bc42b9e10bdc1057
+  ##
+  ## These values might be hashes? in any case they're pretty
+  ## annoying, and I don't see how determinstic they will be
+  ##
+  ## We can easily enough derive sensible names if we need to in the
+  ## index, I think.
+  ##   class-ex-has_link-type-real_type
+  ##   class-ex-has_link-method-f
+  ##   class-ex-has_link-method-g
+  ## Where methods are overloaded it's a bit harder but we could just
+  ## number them really.
+
+  ## So what we want to say here is that the root type exists on this page
+  idx <- doxygen_contents$new(path)
+
+  ## So what we could do here is remap all these ids so that they are
+  ## at least reproducible.  This would be better if we sought out the
+  ## things we actually cared about because then it would be properly
+  ## reproducible.  We'll sort that out later though.
+  re <- "^(.+)([[:xdigit:]]{32})$"
+  ids_from <- c(idx$index$refid, idx$members$member_refid)
+  i <- grep(re, ids_from)
+  ids_to <- ids_from
+  ids_to[i] <- paste0(sub(re, "\\1", ids_from[i]), seq_along(i))
+  ids_to <- paste0("#", ids_to) # all relative now
+  control <- list(
+    link = data.frame(from = ids_from, to = ids_to, stringsAsFactors = FALSE))
+
   x <- extract(path, NULL, contents)[[1]]
-  render_class(x)
+  render_class(x, control)
 
   expect_equal(x$name, "ex::has_link")
 
-  expect_equal(clean_whitespace(render_class(x)), ref)
+  expect_equal(clean_whitespace(render_class(x, NULL)), ref)
 })
 
 
@@ -170,7 +203,7 @@ test_that("function attributes", {
   path <- doxygen_run_one("examples_hpp/function-attributes.hpp")
   contents <- data.frame(kind = "function", name = "ex::add_gpu")
   x <- extract(path, NULL, contents)[[1]]
-  render_function(x)
+  render_function(x, NULL)
 
   expect_equal(x$name, "ex::add_gpu")
 
